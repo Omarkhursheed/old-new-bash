27cc8925b4c972679464f62243bbadefe14989cc
Omarkhursheed <omarkhu@gmail.com>
2018-04-12 15:50:24 +0530

    Down to 11

--- oldnew.sh
+++ oldnew.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
-rm -rf ./old && mkdir ./old
-rm -rf ./new && mkdir ./new
-INDEX=0
+rm -rf ./old && mkdir ./old && rm -rf ./new && mkdir ./new && INDEX=0
 for var in "$@"
 do
 	if [[ $INDEX -eq 0 ]]
 	then
-		startd=$(date -j -f '%d/%m/%Y' "$1" +'%Y-%m-%d');
-		let INDEX=${INDEX}+1
+		startd=$(date -j -f '%d/%m/%Y' "$1" +'%Y-%m-%d') && let INDEX=${INDEX}+1;
 	else
-		cp -r "$var" ./old
-		cp -r "$var" ./new
+		cp -r "$var" ./old && cp -r "$var" ./new
 	fi
 done
-find ./new -type f ! -newermt $startd | xargs rm -rf
-find ./old -type f -newermt $startd | xargs rm -rf
+find ./new -type f ! -newermt $startd | xargs rm -rf && find ./old -type f -newermt $startd | xargs rm -rf
