1567cef89026fc7873c0aed26f946bca10a6f725
Omarkhursheed <omarkhu@gmail.com>
2018-04-12 15:29:11 +0530

    script is standalone now

--- oldnew.sh
+++ oldnew.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-rm -rf ~/old-new-shell/old && mkdir ~/old-new-shell/old
-rm -rf ~/old-new-shell/new && mkdir ~/old-new-shell/new
+rm -rf ./old && mkdir ./old
+rm -rf ./new && mkdir ./new
 INDEX=0
 for var in "$@"
 do
 	if [[ $INDEX -eq 0 ]]
 	then
 		startd=$(date -j -f '%d/%m/%Y' "$1" +'%Y-%m-%d');
-		echo $startd
 		let INDEX=${INDEX}+1
-		echo $INDEX
 	else
-		cp -r "$var" ~/old-new-shell/old
-		cp -r "$var" ~/old-new-shell/new
+		cp -r "$var" ./old
+		cp -r "$var" ./new
 	fi
 done
-touch -t $startd /tmp/$startd
 find ./new -type f ! -newermt $startd | xargs rm -rf
 find ./old -type f -newermt $startd | xargs rm -rf
