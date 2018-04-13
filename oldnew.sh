3f374c513ebff9c69317a1c29fa534e1851ecaef
Omarkhursheed <omarkhu@gmail.com>
2018-04-13 21:15:56 +0530

    completed script

--- oldnew.sh
+++ oldnew.sh
@@ -2,9 +2,9 @@ rm -rf ./old && mkdir ./old && rm -rf ./new && mkdir ./new && INDEX=0
 for var in "$@" 
 do
 	if [[ $INDEX -eq 0 ]]
-		then startd=$(date -j -f '%d/%m/%Y' "$1" +'%Y-%m-%d') && let INDEX=${INDEX}+1 
+		then startd=$(date -j -f '%d/%m/%Y' "$1" +'%Y%m%d') && let INDEX=${INDEX}+1 && startd+="0000"
 	else 
 		cp -r "$var" ./old && cp -r "$var" ./new 
 	fi 
 done
-find ./new -type f ! -newermt $startd | xargs rm -rf && find ./old -type f -newermt $startd | xargs rm -rf
+touch -t $startd /tmp/timestamp && find ./new -type f ! -newer /tmp/timestamp -delete && find ./old -type f -newer /tmp/timestamp -delete  
