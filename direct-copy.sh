9ed3b95dcccdc31de5de90090221363fcd0f4d3e
Omarkhursheed <omarkhu@gmail.com>
2018-04-16 13:19:45 +0530

    update experimental script

--- direct-copy.sh
+++ direct-copy.sh
@@ -4,6 +4,8 @@ do
 	if [[ $INDEX -eq 0 ]]
 		then startd=$(date -j -f '%d/%m/%Y' "$1" +'%Y%m%d') && let INDEX=${INDEX}+1 && startd+="0000"
 	else 
-		touch -t $startd /tmp/timestamp && find "$var" -type f ! -newer /tmp/timestamp | cpio -mdp ./old && find "$var" -type f -newer /tmp/timestamp | cpio -mdp ./new
+		touch -t $startd /tmp/timestamp 
+		find "$var" -type d -exec mkdir -p ./old{} \; && find "$var" -type f ! -newer /tmp/timestamp -exec cp -R {} ./old/{}  \;
+		find "$var" -type d -exec mkdir -p ./new{} \; && find "$var" -type f -newer /tmp/timestamp -exec cp -R {} ./new/{}  \;
 	fi 
 done
